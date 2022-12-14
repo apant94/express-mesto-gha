f352678ee6177e8ccef037083d98fd4504880d16
Anna Panteleeva <apanteleeva94@gmail.com>
2022-12-14 17:39:39 +0300

    feat: настроить апдейт юзера

--- controllers/users.js
+++ controllers/users.js
@@ -3,13 +3,13 @@ const User = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.send({ data: users }))
+    .then((users) => res.send(users))
     .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err.message}` }));
 };
 
 module.exports.getUserById = (req, res) => {
   User.findById(req.params.userId)
-    .then((user) => res.send({ data: user }))
+    .then((user) => res.send(user))
     .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err.message}` }));
 };
 
@@ -19,5 +19,21 @@ module.exports.createUser = (req, res) => {
   // eslint-disable-next-line no-undef
   User.create({ name, about, avatar })
     .then((user) => res.send(user))
+    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err.message}` }));
+};
+
+module.exports.updateProfile = (req, res) => {
+  const { name, about } = req.body;
+
+  User.findByIdAndUpdate(req.user._id, { name, about })
+    .then((user) => res.send(user))
+    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err.message}` }));
+};
+
+module.exports.updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+
+  User.findByIdAndUpdate(req.user._id, { avatar })
+    .then((user) => res.send({ avatar: user.avatar }))
     .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err.message}` }));
 };
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,12 @@
 const userRouter = require('express').Router();
-const { getUsers, createUser, getUserById } = require('../controllers/users');
+const {
+  getUsers, createUser, getUserById, updateProfile, updateAvatar,
+} = require('../controllers/users');
 
 userRouter.get('/users', getUsers);
-userRouter.get('/users:userId', getUserById);
+userRouter.get('/users/:userId', getUserById);
 userRouter.post('/users', createUser);
+userRouter.patch('/users/me', updateProfile);
+userRouter.patch('/users/me/avatar', updateAvatar);
 
 module.exports = userRouter;
