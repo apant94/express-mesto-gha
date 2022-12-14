177d389a8353dddf380159171e22cebd33fdacd2
Anna Panteleeva <apanteleeva94@gmail.com>
2022-12-14 17:59:03 +0300

    feat: настроить лайки карточки

--- controllers/cards.js
+++ controllers/cards.js
@@ -18,3 +18,23 @@ module.exports.deleteCardById = (req, res) => {
     .then(() => res.send('Карточка удалена'))
     .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err.message}` }));
 };
+
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((card) => res.send(card))
+    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err.message}` }));
+};
+
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((card) => res.send(card))
+    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err.message}` }));
+};
