3cb2ebdb4087d1b05bbeb7df855baa61f7df66de
Anna Panteleeva <apanteleeva94@gmail.com>
2022-12-22 17:50:15 +0300

    feat: добавила миддлвэр auth

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 const userRouter = require('express').Router();
+const auth = require('../middlewares/auth');
 const {
   getUsers, createUser, getUserById, updateProfile, updateAvatar, login,
 } = require('../controllers/users');
 
 userRouter.get('/users', getUsers);
 userRouter.get('/users/:userId', getUserById);
-userRouter.post('/signin', login);
+userRouter.post('/signin', auth, login);
 userRouter.post('/signup', createUser);
 userRouter.patch('/users/me', updateProfile);
 userRouter.patch('/users/me/avatar', updateAvatar);
